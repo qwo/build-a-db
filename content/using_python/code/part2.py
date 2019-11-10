f19a0e4d1337ec6943b33d9f30194118ec8bab2e
Stanley Zheng <Stanley@Zheng.nyc>
2019-11-10 01:16:52 -0500

    updating logic

--- content/using_python/code/part2.py
+++ content/using_python/code/part2.py
@@ -3,6 +3,52 @@
 import sys
 import enum
 
+MetaCommandResult = {} #   META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED_COMMAND
+PrepareResult = {}
+#   PREPARE_SUCCESS,
+# PREPARE_UNRECOGNIZED_STATEMENT
+StatementType = {}
+#   STATEMENT_INSERT,
+#  STATEMENT_SELECT
+
+Statement = {}
+# Statement;
+
+def do_meta_command(line):
+  """
+  @returns 0 or {MetaCommandResult}
+  """
+  if line == ".exit":
+    return 0
+  else:
+    return "META_COMMAND_UNRECOGNIZED_COMMAND"
+
+def prepare_statement(line):
+  """
+  @returns {PrepareResult} 
+  or PREPARE_UNRECOGNIZED_STATEMENT
+  """
+  if line == "insert":
+    STATEMENT_TYPE = "STATEMENT_INSERT"
+    return "PREPARE_SUCCESS"
+  elif line == "select":
+    STATEMENT_TYPE = "STATEMENT_SELECT"
+    return "PREPARE_SUCCESS"
+  else:
+    return "PREPARE_UNRECOGNIZED_STATEMENT"
+
+
+def execute_statement(line):
+  """
+  @returns {PrepareResult} 
+  or PREPARE_UNRECOGNIZED_STATEMENT
+  """
+  if line == "STATEMENT_INSERT":
+    print("This is  where we would do an insert.\n")    
+  elif line == "STATEMENT_SELECT":
+    print("This is where we would do a select.\n")
+
+
 def print_prompt():
     print("db > ")
 
@@ -14,8 +60,23 @@ def main(line=None):
     if user_input == ".exit":
       break
 
-    else:
-      print("Unrecognized command '{}'.\n".format(user_input))
+    ## MetaCommand
+    if user_input[0] == '.':
+      meta_command = do_meta_command(user_input)
+      if meta_command == "META_COMMAND_SUCCESS":
+         pass
+      elif meta_command == "META_COMMAND_UNRECOGNIZED_COMMAND":
+        print("Unrecognized command '{}'.\n".format(user_input))
+      continue # EXIT META COMMAND
+
+    ## Statement
+    statement = prepare_statement(user_input)
+    if statement == "PREPARE_SUCCESS":
+      continue
+    elif statement == "PREPARE_UNRECOGNIZED_STATEMENT":
+      print("Unrecognized keyword at start of '{}'.\n".format(user_input))
+    execute_statement(statement)
+    print("Executed.\n")
 
 
 
