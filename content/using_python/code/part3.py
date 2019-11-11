db1c59f8f57463d4639aa3c66dab6a38c8f86023
Stanley Zheng <Stanley@Zheng.nyc>
2019-11-11 15:42:54 -0500

    adding implementation part 3

--- content/using_python/code/part3.py
+++ content/using_python/code/part3.py
@@ -2,6 +2,7 @@
 
 import sys
 
+from dataclasses import dataclass
 from enum import Enum
 
 
@@ -12,8 +13,12 @@ class MetaCommandResult(Enum):
 
 class PrepareResult(Enum):
     PREPARE_SUCCESS = "PREPARE_SUCCESS"
+    PREPARE_SYNTAX_ERROR = "PREPARE_SYNTAX_ERROR"
     PREPARE_UNRECOGNIZED_STATEMENT = "PREPARE_UNRECOGNIZED_STATEMENT"
 
+class ExecuteResult(Enum):
+    EXECUTE_SUCCESS = "EXECUTE_SUCCESS"
+    EXECUTE_TABLE_FULL = "EXECUTE_TABLE_FULL"
 
 class Statement(Enum):
     pass
@@ -24,12 +29,35 @@ class StatementType(Statement):
     STATEMENT_SELECT = "STATEMENT_SELECT"
 
 
+CONST_MAX_PAGES = 1024
+CONST_MAX_ROW_PER_PAGE = 4096
+
+@dataclass
+class Table():
+
+    def __init__(self):
+        self.table_name = "default table"
+        self.pages = CONST_MAX_PAGES
+        self.index = [[]*CONST_MAX_PAGES]
+        self.num_rows = 0
+
+    def insert_row(self, row):
+        page = 0
+        self.index[page].append(row)
+        return "inserted 1 in {}".format(self.table_name)
+    
+    def select(self, line):
+        select = []
+        for i in self.index:
+            select.extend(i)
+        return select
+
 def do_meta_command(line):
     """
   @returns 0 or {MetaCommandResult}
   """
     if line == ".exit":
-        return 0
+        exit(0)
     else:
         return MetaCommandResult.META_COMMAND_UNRECOGNIZED_COMMAND
 
@@ -38,37 +66,53 @@ def prepare_statement(line):
     """
   @returns ( {PrepareResult}, {STATEMENT_TYPE} )
   """
-    if line == "insert":
-        STATEMENT_TYPE = "STATEMENT_INSERT"
+    if "insert" in line:
+        STATEMENT_TYPE = StatementType.STATEMENT_INSERT
+        input_buffer = line.split(" ")
+        if len(input_buffer) != 4:
+            return "PREPARE_SYNTAX_ERROR", None
         return "PREPARE_SUCCESS", STATEMENT_TYPE
-    elif line == "select":
-        STATEMENT_TYPE = "STATEMENT_SELECT"
+    elif "select" in line:
+        STATEMENT_TYPE = StatementType.STATEMENT_SELECT
         return "PREPARE_SUCCESS", STATEMENT_TYPE
     else:
         return "PREPARE_UNRECOGNIZED_STATEMENT", None
 
 
-def execute_statement(line):
+def execute_statement(statement, table, line):
     """
   @returns {PrepareResult} 
   or PREPARE_UNRECOGNIZED_STATEMENT
   """
-    if line == "STATEMENT_INSERT":
-        print("This is  where we would do an insert.\n")
-    elif line == "STATEMENT_SELECT":
+    print(statement)
+    if statement == StatementType.STATEMENT_INSERT:
+        print("This is where we would do an insert.\n")
+        return ExecuteResult.EXECUTE_SUCCESS,         execute_insert(line, table)
+    elif statement == StatementType.STATEMENT_SELECT:
         print("This is where we would do a select.\n")
+        return ExecuteResult.EXECUTE_SUCCESS,         execute_select(line, table)
+
+def execute_insert(line, table):
+    return table.insert_row(line)
+
+
+def execute_select(line, table):
+    return table.select(line)
 
 
 def print_prompt():
     print("db > ")
 
 
+
+table = Table()
 def main(line=None):
     while True:
         print_prompt()
         user_input = line or input()
-        if user_input == ".exit":
-            break
+        user_input = user_input.strip()
+        # if user_input == ".exit":
+        #     break
 
         ## MetaCommand
         if user_input[0] == ".":
@@ -88,8 +132,17 @@ def main(line=None):
             continue
         elif prepare_status == PrepareResult.PREPARE_UNRECOGNIZED_STATEMENT:
             print("Unrecognized keyword at start of '{}'.\n".format(user_input))
-        execute_statement(statement)
-        print("Executed.\n")
+        execute_status, results = execute_statement(statement, table, user_input)
+        
+        if execute_status == ExecuteResult.EXECUTE_SUCCESS:
+            print('executed results:', results)
+            print("Executed.\n");
+        elif  execute_status == ExecuteResult.EXECUTE_TABLE_FULL:
+            print("Error: Table full.\n");
+
+        # Get around to break
+        if line:
+            break
 
 
 if __name__ == "__main__":
